17aa29c6387a11dc47fa0a989008f3a48f0260f6
imba97 <mail@imba97.cn>
2024-10-24 23:52:46 +0800

    style: remove export plugin from cli package

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,1 @@
-export { default as GitHandler } from '@initx-plugin/git'
-export { default as CpHandler } from '@initx-plugin/cp'
-export { default as GpgHandler } from '@initx-plugin/gpg'
-
 export * from '@initx-plugin/core'
