7b370e08237a9d36ffb5e74b4c3d0a868190a4e5
imba97 <mail@imba97.cn>
2025-03-28 00:10:27 +0800

    fix: import fs-extra issue

--- packages/core/src/plugin/manager.ts
+++ packages/core/src/plugin/manager.ts
@@ -1,12 +1,12 @@
 import { c } from '@initx-plugin/utils'
-import { existsSync } from 'fs-extra'
+import fs from 'fs-extra'
 import { resolve } from 'pathe'
 import { NODE_MODULES_DIR, PLUGIN_DIR } from '../constants'
 
 const MANAGER_PLUGIN_NAME = '@initx-plugin/manager'
 
 function detectManager() {
-  return existsSync(
+  return fs.existsSync(
     resolve(PLUGIN_DIR, NODE_MODULES_DIR, MANAGER_PLUGIN_NAME)
   )
 
