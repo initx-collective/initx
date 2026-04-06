a59ad88caac584919021262d86c886ee65f247a6
imba97 <mail@imba97.cn>
2026-04-06 11:15:05 +0800

    feat(utils): migrate to `@inquirer/prompts`

--- packages/utils/src/inquirer.ts
+++ packages/utils/src/inquirer.ts
@@ -1,37 +1,28 @@
-import type { SelectOptions } from './types'
+import type { SelectOptions, SelectReturn } from './types'
 
-import _inquirer from 'inquirer'
+import { confirm as promptConfirm, select as promptSelect } from '@inquirer/prompts'
 
-async function confirm(message: string) {
-  const { result } = await _inquirer.prompt([
-    {
-      type: 'confirm',
-      name: 'result',
-      message
-    }
-  ])
-
-  return result
+async function confirm(message: string): Promise<boolean> {
+  return promptConfirm({
+    message
+  })
 }
 
-async function select(message: string, options: SelectOptions) {
-  const { result } = await _inquirer.prompt([
-    {
-      type: 'list',
-      name: 'result',
-      message,
-      choices: options.map((option, index) => {
-        return typeof option === 'string'
-          ? {
-              name: option,
-              value: index
-            }
-          : option
-      })
-    }
-  ])
-
-  return result
+async function select<T extends SelectOptions>(
+  message: string,
+  options: T
+): Promise<SelectReturn<T>> {
+  return promptSelect({
+    message,
+    choices: options.map((option, index) => {
+      return typeof option === 'string'
+        ? {
+            name: option,
+            value: index
+          }
+        : option
+    })
+  }) as Promise<SelectReturn<T>>
 }
 
 export const inquirer = {
--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -1,6 +1,14 @@
-export interface SelectBaseOption {
+export interface SelectBaseOption<T extends string | number = string | number> {
   name: string
-  value: string | number
+  value: T
 }
 
-export type SelectOptions = string[] | SelectBaseOption[]
+export type SelectOptions<T extends string | number = string | number>
+  = readonly string[] | readonly SelectBaseOption<T>[]
+
+export type SelectReturn<T extends SelectOptions>
+  = T extends readonly string[]
+    ? number
+    : T extends readonly SelectBaseOption<infer V>[]
+      ? V
+      : never
